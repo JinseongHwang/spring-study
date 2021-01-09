6d699860a8d8b4a8b421b2089d67d8335d095ea9
JinseongHwang <eddy5360@naver.com>
2021-01-09 12:12:01 +0900

    feat: MVC development

--- src/main/java/spring/springstudy/controller/MemberController.java
+++ src/main/java/spring/springstudy/controller/MemberController.java
@@ -2,7 +2,13 @@ package spring.springstudy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring.springstudy.domain.Member;
 import spring.springstudy.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,29 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    // URL로 접근하면 여기에 mapping된다.
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // HTML form에서 post 방식으로 데이터가 전달되면 여기서 mapping된다.
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
